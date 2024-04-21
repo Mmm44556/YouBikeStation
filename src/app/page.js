c6b14427e98ebbcf47f1141be039642c64aceb78
Mmm44556 <69757601+Mmm44556@users.noreply.github.com>
2024-04-21 21:34:10 +0800

    no message

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 "use client"
 import { useState } from 'react';
 import Image from "next/image";
-import NavLinks from '@/components/navLinks';
+import NavLinks from '@/components/NavLinks';
 import BurgerMenu from '@/components/BurgerMenu';
 import Logo from '@/assets/logo.png'
 
@@ -15,29 +15,29 @@ export default function Nav() {
     <>
       <div className="flex justify-between items-center gap-10 relative h-[95px]">
         <div className="w-[95px] h-[95px] relative max-lg:w-[65px] max-lg:h-[65px]">
-          <Image alt="logo" src={Logo} fill sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" priority/>
+          <Image alt="logo" src={Logo} fill sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" priority />
         </div>
 
         <div className="lg:hidden max-lg:w-6 max-lg:h-6 flex items-center justify-center cursor-pointer"
           onClick={toggleMenu}>
-          <BurgerMenu isOpen={isOpen}/>
+          <BurgerMenu isOpen={isOpen} />
         </div>
 
         <div className={`flex gap-8 items-center basis-10/12 justify-between max-lg:fixed max-lg:bg-[#B5CC22] bottom-0 
         max-lg:flex-col
-        max-lg:h-[calc(100dvh-6rem)] max-lg:w-dvw max-lg:left-0 max-lg:items-start z-50  ${isOpen ? "max-lg:opacity-100 transition-all duration-300 ease-in-out" :"transition-all duration-300 ease-in-out max-lg:opacity-0 -bottom-full max-lg:z-0"}`}>
+        max-lg:h-[calc(100dvh-6rem)] max-lg:w-dvw max-lg:left-0 max-lg:items-start z-50  ${isOpen ? "max-lg:opacity-100 transition-all duration-300 ease-in-out" : "transition-all duration-300 ease-in-out max-lg:opacity-0 -bottom-full max-lg:z-0"}`}>
 
           <div className='lg:hidden basis-[50%] w-full '>
-    
+
             <div className={`w-[82px] h-[248px]  flex flex-col gap-[32px] mt-8 ms-[32px]  max-lg:ms-16 max-sm:ms-8`}>
-                <NavLinks />
+              <NavLinks />
             </div>
 
           </div>
           <div className="flex gap-10 max-lg:gap-6 max-lg:w-auto w-[520px] h-6 max-lg:hidden">
             <NavLinks />
           </div>
-          
+
           <button
             className=" bg-[#B5CC22] w-[85px] h-[40px] rounded-[100px] px-6 py-[10px] flex items-center ms-[32px] max-lg:w-[81px] max-lg:h-[40px]  max-lg:ms-16 max-sm:ms-8  max-lg:mb-[71px] max-lg:bg-white"
           >
